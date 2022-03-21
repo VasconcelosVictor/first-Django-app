3c44b97d7e932349e0476003a928e230a5f9fce2
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2022-03-21 11:00:46 -0300

    Adding static Files- Part 6

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,5 @@
+from ast import arg
+from asyncio import futures
 from audioop import reverse
 import datetime
 from queue import Queue
@@ -9,6 +11,18 @@ from django.test import TestCase
 from django.utils import timezone
 
 from .models import Question
+
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):  
@@ -96,5 +110,36 @@ class QuestionIndexViewTests(TestCase):
             [question],
         )          
         
-
-   
+    def test_two_past_questions(self):
+        """
+        
+        """
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question2, question1],
+        )
+        
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        the detail view of a question with a pub_date in the future
+        resturns a 404 not found.
+        """        
+        future_question = create_question(question_text="Future question." ,days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        
+        def test_past_question(self):
+            """
+            The detail view of a question with a oub_date in the past
+            displays the question's text.
+            """
+            past_question = create_question(question_text="Past Question.", days=-5)
+            url = reverse('polls:detail', args=(past_question,))
+            response = self.client.get(url)
+            self.assertContains(response, past_question.question_text)
+            
